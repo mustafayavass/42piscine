143bd2dac844c0f0c356dc96ae44233bfd2a7650
Mustafa Yavaş <107552260+mustafayavass@users.noreply.github.com>
2024-02-20 12:55:40 +0300

    Add files via upload

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myavas <myavas@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 11:42:21 by myavas            #+#    #+#             */
+/*   Updated: 2024/02/08 11:42:22 by myavas           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	j = 0;
+	i = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
