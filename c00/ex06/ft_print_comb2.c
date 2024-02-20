539ab621a9a0e534d8243329059aeb86c5eaf55b
Mustafa Yavaş <107552260+mustafayavass@users.noreply.github.com>
2024-02-20 12:51:58 +0300

    Add files via upload

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myavas <myavas@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 11:03:14 by myavas            #+#    #+#             */
+/*   Updated: 2024/01/29 12:08:49 by myavas           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write (1, &a, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = -1;
+	while (a++ < 98)
+	{
+		b = a;
+		while (b++ < 99)
+		{
+			ft_putchar((a / 10) + 48);
+			ft_putchar((a % 10) + 48);
+			ft_putchar(' ');
+			ft_putchar((b / 10) + 48);
+			ft_putchar((b % 10) + 48);
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
